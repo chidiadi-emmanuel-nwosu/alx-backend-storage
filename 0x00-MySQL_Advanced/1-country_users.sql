eac3287d60015f7280e420be3526b19c04a9ccef
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-13 18:48:28 +0100

    my first commit

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- script that creates users table
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE KEY NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
