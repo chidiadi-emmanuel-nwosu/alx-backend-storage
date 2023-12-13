3c71bfa655d6a5f43fe9b4bf09dd23f8c1cead1e
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-13 21:50:39 +0100

    changed old and new

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER email_change
 BEFORE UPDATE ON users
 FOR EACH ROW
     BEGIN
-        IF OLD.email <> NEW.email THEN
+        IF NEW.email <> OLD.email THEN
             SET NEW.valid_email = 0;
         END IF
     END //
