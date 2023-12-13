bfc2670ac462855d282f3880808d8927fd4de681
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-13 20:29:45 +0100

    change AFTER to BEFORE

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,7 +2,7 @@
 -- valid_email only when the email has been changed.
 DELIMITER //
 CREATE TRIGGER reset_valid_email
-AFTER UPDATE ON users
+BEFORE UPDATE ON users
 FOR EACH ROW
     BEGIN
         IF OLD.email <> NEW.email THEN
