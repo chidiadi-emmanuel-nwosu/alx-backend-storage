bf8f5a176abf749db1fdb2a1a35412723b9afde0
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-14 20:10:29 +0100

    added advanced task 100

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,17 @@
+-- script that creates a stored procedure ComputeAverageWeightedScoreForUser
+-- that computes and store the average weighted score for a student.
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN in_user_id INT)
+BEGIN
+    DECLARE total_weight INT;
+    DECLARE weighted_average_score FLOAT;
+
+    SELECT SUM(weight * score) / total_weight INTO weighted_average_score
+    FROM corrections c JOIN projects p ON c.project_id = p.id
+    WHERE user_id = in_user_id;
+
+    UPDATE users
+    SET average_score = weighted_average_score
+    WHERE id = in_user_id;
+END //
+DELIMITER ;
