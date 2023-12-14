768443d74380900d7a0191109a02b1242df9d6e9
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-14 11:47:09 +0100

    fixed error for task 10 - 3

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,14 +2,15 @@
 -- (and returns) the first by the second number or
 -- returns 0 if the second number is equal to 0.
 DELIMITER $$
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10, 2)
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS DECIMAL(10,2)
 BEGIN
-    DECLARE result DECIMAL(10, 2);
+    DECLARE result DECIMAL(10,2);
     IF b = 0 THEN
         SET result = 0;
     ELSE
         SET result = a DIV b;
-    END IF
+    END IF;
     RETURN result;
 END $$
 DELIMITER ;
