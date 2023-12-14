36dbc40440620158bb7a86a8f603ed93244fe5ec
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-14 09:41:23 +0100

    fixed error - 2

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,15 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student.
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN in_user_id INT)
 BEGIN
     DECLARE average_score DECIMAL(10, 2);
 
     SELECT AVG(score) INTO average_score FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = in_user_id;
 
     UPDATE users
     SET average_score = average_score
-    WHERE id = user_id;
+    WHERE id = in_user_id;
 END //
 DELIMITER ;
