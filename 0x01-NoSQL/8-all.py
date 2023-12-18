7a464798ef6d6e04a9c93e87febb488ffc0e19a3
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-18 20:40:29 +0100

    added task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""8-all"""
+
+
+def list_all(mongo_collection):
+    """
+    Retrieve all documents from a MongoDB collection.
+
+    Args:
+    - mongo_collection: A PyMongo collection object
+                        representing the MongoDB collection.
+
+    Returns:
+    - A list of documents from the MongoDB collection.
+      If the collection is empty, an empty list is returned.
+    """
+    if not mongo_collection.count_documents():
+        return []
+    return mongo_collection.find()
