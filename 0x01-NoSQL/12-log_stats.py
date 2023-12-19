6e68989f40629ffd30058f99eb447fd6f2699a1c
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-19 10:56:43 +0100

    fixed task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,22 +2,15 @@
 """ 12-log_stats """
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client: MongoClient = MongoClient('mongodb://127.0.0.1:27017')
+    log_collections = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-client: MongoClient = MongoClient('mongodb://127.0.0.1:27017')
-log_collections = client.logs.nginx
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-print(log_collections.count_documents({}), 'logs')
-print('Methods:')
-for method in methods:
-    print('\tmethod {}: {}'
-          .format(
-              method,
-              log_collections.count_documents({"method": method})
-              )
-          )
-print('{} status checked'
-      .format(
-          log_collections.count_documents({'method': 'GET', 'path': '/status'})
-          )
-      )
+    print(log_collections.count_documents({}), 'logs')
+    print('Methods:')
+    for method in methods:
+        print('\tmethod {}: {}'.format(
+            method, log_collections.count_documents({"method": method})))
+    print('{} status checked'.format(
+        log_collections.count_documents({'method': 'GET', 'path': '/status'})))
