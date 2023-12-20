6dbc66881e5a70f84ed94be5e8eed3517ce73779
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-12-20 20:48:08 +0100

    added count decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -28,6 +28,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores the provided data in Redis with a UUID-based key
--- 0x02-redis_basic/tests/main.py
+++ 0x02-redis_basic/tests/main.py
@@ -18,8 +18,6 @@ print(key)
 local_redis = redis.Redis()
 print(local_redis.get(key))
 
-cache = Cache()
-
 TEST_CASES = {
     b"foo": None,
     123: int,
@@ -29,3 +27,10 @@ TEST_CASES = {
 for value, fn in TEST_CASES.items():
     key = cache.store(value)
     assert cache.get(key, fn=fn) == value
+
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
